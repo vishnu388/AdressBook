9a7d073c538ee638c3095ce1a9074f5f7878f2ca
vishnu <vishnu388@gmail.com>
2021-03-14 21:56:51 +0530

    [vishnu]: Added to person details

---
+++ contact.java
@@ -0,0 +1,31 @@
+package AdressBook;
+
+public class contact {
+	String FIRST_NAME;
+	String LAST_NAME;
+	String ADDRESS;
+	String CITY;
+	String STATE;
+	String POSTAL_CODE;
+	String COUNTRY;
+	String CONTACT_NUMBER;
+	String EMAIL;
+	contact(String fn,String ln,String add,String city,String state,String zip,String country,String mob,String mail)
+	{
+		this.FIRST_NAME=fn;
+		this.LAST_NAME=ln;
+		this.ADDRESS=add;
+		this.CITY=city;
+		this.STATE=state;
+		this.POSTAL_CODE=zip;
+		this.COUNTRY=country;
+		this.CONTACT_NUMBER=mob;
+		this.EMAIL=mail;
+	}
+
+	public String toString() {
+		return String.format(FIRST_NAME+" "+LAST_NAME+" "+ADDRESS+" "+CITY+" "+STATE+" "+POSTAL_CODE+" "
+									 +COUNTRY+" "+CONTACT_NUMBER+" "+EMAIL);
+	}
+
+}
