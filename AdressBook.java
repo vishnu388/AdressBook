a415128db7abec0473733c157e2e30562976c32f
vishnu <vishnu388@gmail.com>
2021-03-14 22:36:23 +0530

    [vishnu]: Added to edit the person details

--- AdressBook.java
+++ AdressBook.java
@@ -27,13 +27,42 @@ public class AdressBook {
 		String mail=sc.next();
 		contactList[increment++]=new  contact(fn, ln, add, city, state,zip,country,mob,mail);
 		return increment;
-
+	 }
+	void Edit(int increment)
+	{
+		int i;
+		for(i=0;i<10;i++)
+		{
+			if(i==increment)
+			{
+			
+				System.out.println("Enter First Name:");
+			 	String fn=sc.next();
+      		System.out.println("Enter last name:");
+      		String ln=sc.next();
+      		System.out.println("Enter Address:");
+      		String add=sc.next();
+      		System.out.println("Enter City:");
+      		String city=sc.next();
+      		System.out.println("Enter State:");
+      		String state=sc.next();
+      		System.out.println("Enter postal_code:");
+      		String zip=sc.next();
+      		System.out.println("Enter Country:");
+      		String country=sc.next();
+      		System.out.println("Enter contact number:");
+      		String mob=sc.next();
+      		System.out.println("Enter email :");
+      		String mail=sc.next();
+      		contactList[increment++]=new  contact(fn, ln, add, city, state,zip,country,mob,mail);
+				break;
+			}
+		}
+		System.out.println("Your changes added to the AddressBook successfully");
 	}
-
 	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
 		AdressBook vishnu=new AdressBook();
-		vishnu.toString();
-		System.out.println("Adding new person info");
+		vishnu.Edit(0); 
 	}
-
 }
