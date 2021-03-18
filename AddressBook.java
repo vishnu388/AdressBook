4c86da6bd3526ef92a1c74fec230cdd02887c57a
vishnu <vishnu388@gmail.com>
2021-03-18 19:25:31 +0530

    [vishnu]:Added to Unique Numbers

--- AddressBook.java
+++ AddressBook.java
@@ -1,54 +1,114 @@
 package AddressBook;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
 public class AddressBook {
-	static List listing = new ArrayList();
-	public static Scanner sc = new Scanner(System.in);
-	static AddressBook vishnu = new AddressBook();
-	static  final int i=0;
-	 public void addContact() {
-		System.out.println("Enter numbers of  persons you want to add");
-		int numOfPerson=sc.nextInt();
-	    for(int i=0; i<=numOfPerson; i++)
-	    {
-	    	System.out.println("\nPerson :  "+i+" Enter First Name");
-	        String fn= sc.next();
-	        sc.nextLine();
-	        System.out.println("Person : " +i+ " Enter Last Name");
-	        String ln = sc.next();
-	        sc.nextLine();
-	        System.out.println("Person : " +i+ " Enter Address");
-	        String add = sc.next();
-	        sc.nextLine();
-	        System.out.println("Person : " +i+ " Enter City");
-	        String city = sc.nextLine();
-	        System.out.println("Person : " +i+ " Enter State");
-	        String state = sc.next();
-	        sc.nextLine();
-	        System.out.println("Person : " +i+ " Enter postal_Code");
-	        String zip = sc.next();
-	        System.out.println("Person : " +i+ " Enter Country_Name");
-	        String country = sc.next();
-	        System.out.println("person :" +i+ "Enter Mobile number");
-	        String mob=sc.next();
-	        System.out.println("Person : " +i+ " Enter E-mail");
-	        String mail = sc.next();
-	        contact contact = new contact(fn, ln, add, city, state, zip,country,mob, mail);
-	        listing.add(contact);
-	          
-	    }
-	   
-}
-	   
-public static void main(String args[]) {
-	
-	vishnu.addContact();	
-	System.out.println("The person records are listed below");
-	System.out.println(listing);
-	System.out.println("number of  contacts added sccessfully");
-	}
+	public int indexValue = 1;
+    public HashMap<Integer,contact> contacts = new HashMap<>();
+    public static Scanner sc = new Scanner(System.in);
+    static AddressBook vishnu = new AddressBook();
+
+    public void addContact () {
+        System.out.println("Enter Number of person you want to add");
+        int numOfPerson = sc.nextInt();
+        for (int add = 1; add <= numOfPerson; add++){
+        System.out.println("\nPerson : " + add+ " Enter First Name");
+        String first = sc.next();
+        sc.nextLine();
+        System.out.println("Person : " + add+ " Enter Last Name");
+        String last = sc.next();
+        sc.nextLine();
+        System.out.println("Person : " + add+ " Enter Address");
+        String address = sc.next();
+        sc.nextLine();
+        System.out.println("Person : " + add+ " Enter City");
+        String city = sc.nextLine();
+        System.out.println("Person : " + add+ " Enter State");
+        String state = sc.next();
+        sc.nextLine();
+        System.out.println("Person : " + add+ " Enter Zip Code");
+        int zip = sc.nextInt();
+        System.out.println("Person : " + add+ " Enter Phone Number");
+        long phone = sc.nextLong();
+        System.out.println("Person : " + add+ " Enter E-mail");
+        String email = sc.next();
+        contact contact = new contact(First, last, address, city, state, zip, phone, email);
+        contacts.put(indexValue, contact);
+        indexValue++;
+        }
+        System.out.println("\nContacts added Successfully");
+    }
+
+    public Boolean check(String checkName) {
+        if (contacts.isEmpty())
+                return false;
+        else {
+            System.out.println("\nAdd contact again with different first name.");
+            Iterator<Integer> itr = contacts.keySet().iterator();
+            while (itr.hasNext()) {
+                int key = itr.next();
+                if (contacts.get(key).FirstName.equals(checkName)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public void deleteContact() {
+        if (contact.isEmpty()) {
+            System.out.println("Contact list is empty.");
+        } else {
+            System.out.println("Enter the first name to delete contact.");
+            String name = sc.next();
+            Iterator<Integer> itr = contacts.keySet().iterator();
+            while(itr.hasNext()) {
+                int key = itr.next();
+                if (contacts.get(key).FirstName.equals(name)) {
+                    contacts.remove(key);
+                    System.out.println("Contact deleted with first name : "+name);
+                }
+            }
+        }
+    }
+   
+    public static void main (String [] args){
+ 
+    	System.out.println("---------------WELCOME TO THE ADDRESS_BOOK_MANAGEMENT SYSTEM-------------"); 
+    	Scanner sc=new Scanner(System.in);
+    	int increment=0;
+    	System.out.println("Enter valid option to perform Book-Manager_Application(1-->ENTER,2-->EXIT):");
+    	int option=sc.nextInt();
+    		while(option !=2) {
+    			System.out.println("select the operations(1,2,3) you want to perform 1)Add 2)Delete \n");
+    			int choose =sc.nextInt();
+    			switch(choose) 
+    			{
+    				case 1:
+    					System.out.println("Adding credentials to the Application");
+    					  vishnu.addContact();
+    					break;
+    				
+    				case 2:
+    					System.out.println("currently you are in Delete-mode ,once you delete it's not revert back \n");
+    					 vishnu.deleteContact();
+    					break;
+    				
+    				default:
+    					System.out.println("select valid operation[1/2]");
+    			}
+    		System.out.println("Enter choice(1-Enter/2-Exit):");
+    		int option1=sc.nextInt();
+    		if (option1==2)
+    			{
+    				System.out.println("OOPS!! you are choosing an Exit mode");
+    				break;
+    			}
+    			}
+    		System.out.println("THANKS FOR USING BOOK MANAGER-APPLICATION");
+    	}
 
 }
