74f63f3ccd0fe04de452a96fcaa9de047e58be1f
vishnu <vishnu388@gmail.com>
2021-03-25 18:52:47 +0530

    [vishnu]:Added to sorting Alphabatical

---
+++ AddressBookList.java
@@ -0,0 +1,20 @@
+public class AddressBookList {
+    public String userInputBookName;
+
+    public AddressBookList(String userInputBookName)
+    {
+        this.userInputBookName = userInputBookName;
+    }
+    public String getUserInputBookName()
+    {
+        return  userInputBookName;
+    }
+    public void setUserInputBookName(String firstName)
+    {
+        this.userInputBookName = userInputBookName;
+    }
+    public String toString()
+    {
+        return "AddressBookName:" + userInputBookName + "\n";
+    }
+}
