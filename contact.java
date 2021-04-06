c41ce551d166262fee660f69e296f30415efb11e
vishnu <vishnu388@gmail.com>
2021-04-06 19:06:27 +0530

    [vishnu]:Added to contact details for IO Files

--- contact.java
+++ contact.java
@@ -7,86 +7,83 @@ public class contact {
     String email;
     long mobileNo;
     int zip;
-
-
-    public contact(String firstName, String lastName, String address, String city, String state, String email, long mobileNo, int zip) {
+    public contact(String firstName, String lastName, String address, String city, String state, String email, long mobileNo, int zip)
+    {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.address = address;
+        this.address= address;
         this.city = city;
         this.state = state;
         this.email = email;
         this.mobileNo = mobileNo;
         this.zip = zip;
-
     }
-
-    public String getFirstName() {
+    public String getFirstName()
+    {
         return firstName;
     }
-
-    public void setFirstName(String firstName) {
+    public void setFirstName(String firstName)
+    {
         this.firstName = firstName;
     }
-
-    public String getLastName() {
+    public String getLastName()
+    {
         return lastName;
     }
-
-    public void setLastName(String lastName) {
+    public void setLastName(String lastName)
+    {
         this.lastName = lastName;
     }
-
-    public String getAddress() {
+    public String getAddress()
+    {
         return address;
     }
-
-    public void setAddress(String address) {
+    public void setAddress(String address)
+    {
         this.address = address;
     }
-
-    public String getCity() {
+    public String getCity()
+    {
         return city;
     }
-
-    public void setCity(String city) {
+    public void setCity(String city)
+    {
         this.city = city;
     }
-
     public String getState() {
         return state;
     }
-
-    public void setState(String state) {
+    public void setState(String state)
+    {
         this.state = state;
     }
-
-    public String getEmail() {
+    public String getEmail()
+    {
         return email;
     }
-
-    public void setEmail(String email) {
+    public void setEmail(String email)
+    {
         this.email = email;
     }
-
-    public long getMobileNo() {
+    public long getMobileNo()
+    {
         return mobileNo;
     }
-
-    public void setMobileNo(long mobileNo) {
+    public void setMobileNo(long mobileNo)
+    {
         this.mobileNo = mobileNo;
     }
-
-    public int getZip() {
+    public int getZip()
+    {
         return zip;
     }
-
-    public void setZip(int zip) {
+    public void setZip(int zip)
+    {
         this.zip = zip;
     }
-
-    public String toString() {
-        return "Contact [firstName=" + firstName + ", lastName=" + lastName + ", Address=" + address + ", city=" + city
+    public String toString()
+    {
+        return "Contact [firstName=" + firstName + ", lastName=" + lastName + ", Address="+address+", city=" + city
                 + ", state=" + state + ", pincode=" + zip + ", MobileNo=" + mobileNo + "]" + "\n";
     }
 }
